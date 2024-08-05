9aca878e492b873bd91a0bbe0f98d2565ce9be0a
renanrcrr <renanrcrr.developer@gmail.com>
2024-08-05 23:11:47 +0930

    Create the first operation.

--- src/main/java/io/rcrr/springboot/SpringbootApplication.java
+++ src/main/java/io/rcrr/springboot/SpringbootApplication.java
@@ -1,13 +1,31 @@
 package io.rcrr.springboot;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.rcrr.springboot.domain.Category;
+import io.rcrr.springboot.repositories.CategoryRepository;
+
 @SpringBootApplication
-public class SpringbootApplication {
+public class SpringbootApplication implements CommandLineRunner{
+	@Autowired
+	CategoryRepository categoryRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootApplication.class, args);
+	}
+
+	// Creates objects in runtime to be save in the database
+	@Override
+	public void run(String... args) throws Exception {
+		Category cat1 = new Category(null, "Technology");
+		Category cat2 = new Category(null, "Office");
+		
+		categoryRepository.saveAll(Arrays.asList(cat1, cat2));
 	}
 
 }

b8da2a357e70efd1245726ea1f937dc7a9b89d3f
renanrcrr <renanrcrr.developer@gmail.com>
2024-08-10 14:56:45 +0930

    Adding Address and Client entities.

---
+++ src/main/java/io/rcrr/springboot/domains/Address.java
@@ -0,0 +1,118 @@
+package io.rcrr.springboot.domains;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="Endereco")
+public class Address implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Integer id;
+	private String street;
+	private String number;
+	private String completement;
+	private String neighborhood;
+	private String postalCode;
+	
+	@ManyToOne
+	@JoinColumn(name="client_id")
+	private Client client;
+	
+	public Address() {}
+
+	public Address(Integer id, String street, String number, String completement, String neighborhood,
+			String postalCode, Client client) {
+		super();
+		this.id = id;
+		this.street = street;
+		this.number = number;
+		this.completement = completement;
+		this.neighborhood = neighborhood;
+		this.postalCode = postalCode;
+		this.client = client;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Address other = (Address) obj;
+		return Objects.equals(id, other.id);
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getStreet() {
+		return street;
+	}
+
+	public void setStreet(String street) {
+		this.street = street;
+	}
+
+	public String getNumber() {
+		return number;
+	}
+
+	public void setNumber(String number) {
+		this.number = number;
+	}
+
+	public String getCompletement() {
+		return completement;
+	}
+
+	public void setCompletement(String completement) {
+		this.completement = completement;
+	}
+
+	public String getNeighborhood() {
+		return neighborhood;
+	}
+
+	public void setNeighborhood(String neighborhood) {
+		this.neighborhood = neighborhood;
+	}
+
+	public String getPostalCode() {
+		return postalCode;
+	}
+
+	public void setPostalCode(String postalCode) {
+		this.postalCode = postalCode;
+	}
+
+	public Client getClient() {
+		return client;
+	}
+
+	public void setClient(Client client) {
+		this.client = client;
+	}
+}
---
+++ src/main/java/io/rcrr/springboot/domains/Client.java
@@ -0,0 +1,119 @@
+package io.rcrr.springboot.domains;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Objects;
+import java.util.Set;
+
+import io.rcrr.springboot.domains.enums.ClientType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="Cliente")
+public class Client implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Integer id;
+	private String name;
+	private String email;
+	private String cpfOrCnpj;
+	private ClientType clientType;
+	
+	@OneToMany(mappedBy="client")
+	private List<Address> addresses = new ArrayList<>();
+	
+	private Set<String> telephones = new HashSet<>();
+	
+	public Client() {}
+
+	public Client(Integer id, String name, String email, String cpfOrCnpj, ClientType clientType) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.email = email;
+		this.cpfOrCnpj = cpfOrCnpj;
+		this.clientType = clientType;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getCpfOrCnpj() {
+		return cpfOrCnpj;
+	}
+
+	public void setCpfOrCnpj(String cpfOrCnpj) {
+		this.cpfOrCnpj = cpfOrCnpj;
+	}
+
+	public ClientType getClientType() {
+		return clientType;
+	}
+
+	public void setClientType(ClientType clientType) {
+		this.clientType = clientType;
+	}
+
+	public List<Address> getAddresses() {
+		return addresses;
+	}
+
+	public void setAddresses(List<Address> addresses) {
+		this.addresses = addresses;
+	}
+
+	public Set<String> getTelephones() {
+		return telephones;
+	}
+
+	public void setTelephones(Set<String> telephones) {
+		this.telephones = telephones;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Client other = (Client) obj;
+		return Objects.equals(id, other.id);
+	}
+}
